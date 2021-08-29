26c368dbdf8dd6c11a34db7f08973bd898ca7cc5
quhaast <asndjabbd@gmail.com>
2021-08-29 17:11:31 +0300

    Checking parameter in 3rd task

--- week2/ex3.c
+++ week2/ex3.c
@@ -3,18 +3,23 @@
 
 int main(int argc, char* argv[]) {
     int size;
-    sscanf(argv[1], "%d", &size);
+    
+    if (argc == 1) {
+	printf("No figure size was provided.\n");
+	} else {
+		sscanf(argv[1], "%d", &size);
 
-    // printing the triangle
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 2 * size; j++) {
-            if (j + i < size || j - i > size) { // check if the space symbol or star should appear
-                printf(" ");
-            } else {
-                printf("*");
-            }
-        }
-        printf("\n");
+		// printing the triangle
+		for (int i = 0; i < size; i++) {
+		    for (int j = 0; j < 2 * size; j++) {
+		        if (j + i < size || j - i > size) { // check if the space symbol or star should appear
+		            printf(" ");
+		        } else {
+		            printf("*");
+		        }
+		    }
+		    printf("\n");
+		}
     }
 
     return 0;
